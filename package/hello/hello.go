66620d745fe9d4fefbbcab92f5e7909242d212fe
Hyunsang <asunhs@gmail.com>
2015-08-16 16:34:37 +0900

    local package

--- package/hello/hello.go
+++ package/hello/hello.go
@@ -2,11 +2,15 @@ package main
 
 // 실패함
 // GOPATH 쫌 귀찮네?
+// 상대 경로로 참조할 수 있구나 결국 비슷하네
+// 책에서는 글로벌 패키지 참조만 설명한거네
 import (
-    "calc"
+    "../calc"
+    "../test/memo"
     "fmt"
 )
 
 func main() {
     fmt.Println(calc.Sum(2, 3))
+    fmt.Println(memo.Memo("Sun"))
 }
---
+++ package/test/memo/memo.go
@@ -0,0 +1,5 @@
+package memo
+
+func Memo(s string) string {
+    return "Hello, " + s
+}
